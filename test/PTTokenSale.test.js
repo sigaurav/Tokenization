2126606a0e69454c96a4516712cf1ca3c2d7051c
sigaurav <grv.skrwr0562@gmail.com>
2022-04-30 03:11:24 +0530

    Added Unit Test for Token Sale contract

--- test/PTTokenSale.test.js
+++ test/PTTokenSale.test.js
@@ -13,7 +13,23 @@ contract("TokenSale Test", async(accounts) => {
 
     it("Should not have any tokens left in deployer account", async() => {
         let instance = await Token.deployed();
-        return await expect(instance.balanceOf(deployerAccount)).to.eventually.be.a.bignumber.equal(new BN(0));
-    })
+        return await expect(instance.balanceOf.call(deployerAccount)).to.eventually.be.a.bignumber.equal(new BN(0));
+    });
+
+    it("All tokens should be in the PTTokensale constract by default", async() => {
+        let instance = await Token.deployed();
+        let balanceOfSaleContract = await instance.balanceOf.call(PTTokenSale.address);
+        let totalSupply = await instance.totalSupply.call();
+        return await expect(balanceOfSaleContract).to.be.a.bignumber.equal(totalSupply);
+    });
+
+    it("Should be able to buy tokens by simply sending ether to contract", async() => {
+        let instance = await Token.deployed();
+        let saleInstance = await PTTokenSale.deployed();
+        let balanceBeforeTransaction = await instance.balanceOf.call(recipient);
+
+        await expect(saleInstance.sendTransaction({from: recipient, value: web3.utils.toWei("1", "wei")})).to.be.fulfilled;
+        return expect(balanceBeforeTransaction + 1).to.be.bignumber.equal(await instance.balanceOf.call(recipient));
+    });
 
 });
